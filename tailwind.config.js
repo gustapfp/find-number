9d4aa8c5e57680d6115361c12851143f49bf2945
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2023-02-02 13:56:54 -0300

    index.html first version done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        xiketic: "#1A1423",
+        dark_purple: "#372549",
+        twilight_lavender: "#774C60", 
+        popstar: "#B75D69",
+        unbleached_silk : "#EACDC2"
+      },
+    },
   },
   plugins: [],
 }
