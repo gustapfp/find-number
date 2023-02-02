9b304558939b9baacac0c71814436c48e51f5cd2
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2023-02-02 14:50:31 -0300

    secret number generated

---
+++ src/app/secretNumber.js
@@ -0,0 +1,14 @@
+function generateRandom(min_value, max_value) {
+    let secret_number = Math.random() * (max_value - min_value) + 1;
+    return [parseInt(secret_number), min_value, max_value];
+}
+
+const [secret_number, min_value, max_value] = generateRandom(0, 1000)
+
+const display_min_value = document.getElementById("min-value")
+const display_max_value = document.getElementById("max-value")
+
+display_max_value.innerHTML = max_value
+display_min_value.innerHTML = min_value
+
+console.log(max_value)
